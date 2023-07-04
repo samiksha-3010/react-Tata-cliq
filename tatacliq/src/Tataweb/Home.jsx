19d6ba79d5c5eb4bea3b473a04519360e6014c7f
samiksha-3010 <samiksha30102@gmail.com>
2023-07-04 07:45:07 +0530

    done

--- tatacliq/src/Tataweb/Home.jsx
+++ tatacliq/src/Tataweb/Home.jsx
@@ -1,9 +1,122 @@
 import React from 'react'
-
+import './Home.css'
 const Home = () => {
 
   return (
-    <div>Home</div>
+
+    <>
+        <div id='Home'>  
+        <div id='maincontainer'>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855656478.jpg'/>
+         </div>
+         <div id='container2'>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596848218142.jpg'/>
+          </div>
+          <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47575793303582.jpg'/>
+          </div>
+          <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466349598.jpg'/>
+          </div>
+          <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466349598.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466349598.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466546206.jpg'/>
+          </div>
+          <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466611742.jpg'/>
+          </div>
+          <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466611742.jpg'/>
+          </div>
+          <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47548678373406.jpg'/>
+          </div>
+          <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466152990.jpg'/>
+          </div>
+         </div>
+         
+         
+          <div>
+
+          <div id='container3'>
+              <div>
+                <img src='https://assets.tatacliq.com/medias/sys_master/images/46762762436638.jpg'/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855787550.jpg'/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/46690782937118.jpg'/>
+              </div>
+          </div>
+          <div id='container4'>
+              <div>image..... </div>
+              <div id='image2'> 
+               <div>
+                <img src='https://assets.tatacliq.com/medias/sys_master/images/47596857851934.jpg'/>
+               </div>
+               <div>
+               <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865257502.jpg'/>
+               </div>
+               <div>
+               <img src='https://assets.tatacliq.com/medias/sys_master/images/47596857983006.jpg'/>
+               </div>
+               <div>
+               <img src='https://assets.tatacliq.com/medias/sys_master/images/47596866830366.jpg'/>
+               </div>
+              </div>
+            </div>
+            <div id='container5'>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344211486.jpg'/> </div>
+                 <div id='divimg2'>
+                  <div>
+                    <img src='https://assets.tatacliq.com/medias/sys_master/images/47596650758174.jpg'/>
+                  </div>
+                  <div>
+                    <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853493790.jpg'/>
+                  </div>
+                  <div>
+                    <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853690398.jpg'/>
+                  </div>
+                  <div>
+                  <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865323038.jpg'/>
+                  </div>
+
+                
+            </div>
+            <div id='container6'>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344277022.jpg'/>
+            </div>
+            <div id='divimag3'>
+              <div>
+                <img src='https://assets.tatacliq.com/medias/sys_master/images/47513577619486.jpg'/>
+              </div>
+              <div>
+                <img src=''/>
+              </div>
+              <div>
+                <img src=''/>
+              </div>
+              <div>
+                <img src=''/>
+              </div>
+
+                     {/* <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344408094.jpg'/> */}
+                     {/* <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344342558.jpg'/> */}
+            </div>
+
+          </div>
+        </div>
+
+    </>
+
   )
   
 }
