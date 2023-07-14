6b64de7395d0be7720a01fc553140a6e32dbc45e
samiksha-3010 <samiksha30102@gmail.com>
2023-07-14 15:09:21 +0530

    Update Women.jsx

--- tatacliq/src/Tataweb/Women.jsx
+++ tatacliq/src/Tataweb/Women.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 
-const Women = () => {
+function Women () {
 
     const router = useNavigate()
     function WomenSingle (){
